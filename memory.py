7c9e431bccf8027c3235fb83c23737220b7ed5b6
KiurB <119136858+KiurB@users.noreply.github.com>
2025-03-03 10:12:48 +0100

    Add files via upload

---
+++ memory.py
@@ -0,0 +1,26 @@
+#Author: Kiur
+import torch.utils.data as D
+import torch
+
+class StaticData(D.Dataset):
+    """
+    A object to store the ENI dataset for training RL algorithms.
+    """
+
+    def __init__(self, state_s, act_s, rw_s):
+        
+        self.states = state_s
+        self.acts = act_s
+        self.rws = rw_s
+    
+    def __len__(self):
+        return len(self.states)-1
+
+    def __getitem__(self,idx):
+
+        sample = {"state": self.states[idx],
+                  "action": self.acts[idx],
+                  "reward": self.rws[idx+1],
+                  "n_state": self.states[idx+1],
+                  "n_action": self.acts[idx+1]}
+        return sample
